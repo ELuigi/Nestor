ee04dcd78d9814d9ee41a65d24abe596d554d4c3
etienneluigi <etienne.luigi@gmail.com>
2017-10-19 21:04:46 +0200

    clean indentation

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,18 @@
 
 DigitalOut ActivityLed(PA_5);
 Motor MainMotor(MotorLeftForward,MotorLeftBackward,MotorRightForward,MotorRightBackward);
+
+
 //specify period duration
 
 int main() {
     while(1)
     {
-       ActivityLed = 1; // LED is ON
-       MainMotor.TurnLeft(0.5);
-       wait(10); // 200 ms
-       ActivityLed = 0;
-       MainMotor.TurnRight(0.5);
+      ActivityLed = 1; // LED is ON
+      MainMotor.TurnLeft(0.5);
+      wait(10); // 200 ms
+      ActivityLed = 0;
+      MainMotor.TurnRight(0.5);
       wait(10); // 1 sec
       ActivityLed = 1;
       MainMotor.MoveFront(0.5);
