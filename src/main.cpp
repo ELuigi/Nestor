e2ef61a37e769dc0060661bbda211f84354486cc
Etienne LUIGI <ELG@softcomputing.corp>
2017-10-20 22:34:32 +0200

    Modify code for Pwm read test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <mbed.h>
 #include <target.h>
 #include <Motor.h>
+#include <RC_PWM.h>
 
 DigitalOut ActivityLed(PA_5);
 Motor MainMotor(MotorLeftForward,MotorLeftBackward,MotorRightForward,MotorRightBackward);
 Serial Computer(USBTX, USBRX);
+RcPwm pwmrc(PB_10);
 
 //specify period duration
 
@@ -14,16 +16,9 @@ int main() {
     while(1)
     {
       ActivityLed = 1; // LED is ON
-      MainMotor.TurnLeft(0.5);
-      wait(10); // 200 ms
-      ActivityLed = 0;
       MainMotor.TurnRight(0.5);
-      wait(10); // 1 sec
-      ActivityLed = 1;
-      MainMotor.MoveFront(0.5);
-      wait(10); // 1 sec
-      ActivityLed = 0;
-      MainMotor.MoveBack(0.5);
-      wait(10); // 1 se
+      Computer.printf("period %f",pwmrc.period());
+      Computer.printf("pulse : %f",pwmrc.pulsewidth());
+      
     }
 }
