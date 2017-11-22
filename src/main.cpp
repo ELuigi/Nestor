b2d4c019e463940aa690e414ed819d41a9d61b27
etienneluigi <etienne.luigi@gmail.com>
2017-11-22 20:58:24 +0100

    modify for test of new pwm setup

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,28 @@
 DigitalOut ActivityLed(PA_5);
 Motor MainMotor(MotorLeftForward,MotorLeftBackward,MotorRightForward,MotorRightBackward);
 Serial Computer(USBTX, USBRX);
-RcPwm pwmrc(PB_10);
+RcPwm pwmrc(PC_5);
 
 //specify period duration
 
 int main() {
-  Computer.baud(9600);
+
+  Computer.baud(115200);
   Computer.printf("Starting communication...");
+TIM_HandleTypeDef TIM_2;
+
+
     while(1)
     {
       ActivityLed = 1; // LED is ON
       MainMotor.TurnRight(0.5);
-      Computer.printf("period %f",pwmrc.period());
-      Computer.printf("pulse : %f",pwmrc.pulsewidth());
-      
+      Computer.printf("period %f ",pwmrc.period());
+      Computer.printf("pulse : %f \n",pwmrc.pulsewidth());
+
+      wait(2);
+      MainMotor.TurnRight(0.8);
+      Computer.printf("period %f ",pwmrc.period());
+      Computer.printf("pulse : %f \n",pwmrc.pulsewidth());
+      wait(2);
     }
 }
